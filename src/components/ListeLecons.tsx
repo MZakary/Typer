ee5413ee34266bbc42b2f0e95738fc53215afa5d
MZakary <zakary-mitrovic@hotmail.ca>
2025-06-25 13:23:08 -0400

    changed some stuff real quick

--- src/components/ListeLecons.tsx
+++ src/components/ListeLecons.tsx
@@ -11,9 +11,9 @@ function ListeLecons(){
             <h1 className="title" autoFocus>Liste des leçons</h1>
             <p>Nous vous invitons à choisir parmis la liste des leçons suivantes:</p>
             <ul className="listeContainer">
-                <li className="listeItem"><Link to={'/lecon1'}>Leçon 1: La rangée de base (a, s, d, f, j, k, l, et le point-virgule)</Link></li>
-                <li className="listeItem"><Link to={'/lecon2'}>Leçon 2: Lettre e et i</Link></li>
-                <li className="listeItem"><Link to={'/lecon3'}>Leçon 3: Lettre R</Link></li>
+                <li className="listeItem"><Link to={'/lecon1'}>Leçon 1</Link></li>
+                <li className="listeItem"><Link to={'/lecon2'}>Leçon 2</Link></li>
+                <li className="listeItem"><Link to={'/lecon3'}>Leçon 3</Link></li>
             </ul>
         </section>
     )
